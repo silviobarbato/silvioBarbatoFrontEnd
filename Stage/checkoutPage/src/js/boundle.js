fd812db68602d00f10e504a73f432d97254f62e8
silviobarbato <silvio.barbato@soprasteria.com>
2019-02-04 18:23:27 +0100

    fixed right-content and breadcrumb

--- Stage/checkoutPage/src/js/boundle.js
+++ Stage/checkoutPage/src/js/boundle.js
@@ -11059,41 +11059,102 @@ $( document ).ready(function() {
         //$(this).parent().parent().find(".shipping-mode-title").css("background-color", "blue");
         $(this).closest('.shipping-mode').find(".shipping-mode-title").css("background-color", "#0061af");//cambia colore al titolo
 
-
-        if($(this).find('.check1').find('input').is(':checked')){
-            $(this).find('.check1').find('input').prop('checked', false);
-        }else{
-            $(this).find('.check1').find('input').prop('checked', true);
-        }
-
-        //$(this).find(".check1").find('input').prop("checked", true);//dà la proprietà checked alla checkbox
-        $(this).closest('.shipping').find('.shipping-address').find('.shipping-address-form').css("display", "block");//display block al div contenitore della form
-
         $(this).parent().css("display", "none"); //fa scomparire la scelta del tipo di ritiro
 
+        if($(this).hasClass('choose1')){
+            let temp = $('#temp-mustache').html();
+            let html = mustache.render(temp);
+            $('.shipping-address-form').html(html);
+            $(this).closest('.shipping-mode').find('.shipping-store-confirm').css("display", "none");
+            $(this).closest('.shipping-mode').find('.shipping-mode-confirm').css("display", "block");
 
-        $(this).closest('.shipping-mode').find('.shipping-mode-confirm').css("display", "block");//mi fa comparire il tipo di spedizione scelto
+            $(this).find(".check1").find('input').prop("checked", true);//dà la proprietà checked alla checkbox
 
+            $(this).closest('.shipping').find('.shipping-address').find('.shipping-address-form').css("display", "block");
+            $(this).closest('.shipping').find('.shipping-address').find('.choose-country').css('display', 'none');
 
+            /*---------------------------------*/
+            $(this).closest('.container').find('.step-one').css('width', '33.33%');
+            $(this).closest('.container').find('.step-second').css('width', '33.33%');
+            $(this).closest('.container').find('.step-second').addClass('change');
+            $(this).closest('.container').find('.step-three').css('display', 'block');
+            /*------------------------------------*/
 
-        let temp = $('#temp-mustache').html();
-        let html = mustache.render(temp);
-        $('.shipping-address-form').html(html); //rendering form spedizione
+            /*Set text span*/
+            $(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-left').text('Spese di spedizione');
+            $(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-right').text('€ 4,99');
+            /*--------------------*/
 
+            if($(this).parent().find('.choose2').find('input').is(':checked')){
+                $(this).parent().find('.choose2').find('input').prop('checked', false);
+                $(this).parent().find('.choose2').removeClass('play');
+            }
 
-        let temp2 = $('#temp-choose-confirm').html();
-        let html2 = mustache.render(temp2);
-        $('.shipping-mode-confirm').html(html2);//rendering tipo di spedizione scelta
+            let temp2 = $('#temp-choose-confirm').html();
+            let html2 = mustache.render(temp2);
+            $('.shipping-mode-confirm').html(html2);
+
+        }else{
+            if($(this).hasClass('choose2')){
+                let temp3 = $('#temp-choose-store').html();
+                let render = mustache.render(temp3);
+                $('.shipping-store-confirm').html(render);
+
+                $(this).closest('.shipping-mode').find('.shipping-store-confirm').css("display", "block");
+                $(this).closest('.shipping-mode').find('.shipping-mode-confirm').css("display", "none");
+
+                $(this).closest('.shipping').find('.shipping-address').find('.shipping-address-form').css("display", "none");
+                $(this).closest('.shipping').find('.shipping-address').find('.choose-country').css("display", "block");
+
+                $(this).find(".check1").find('input').prop("checked", true);//dà la proprietà checked alla checkbox
+
+                if($(this).parent().find('.choose1').find('input').is(':checked')){
+                    $(this).parent().find('.choose1').find('input').prop('checked', false);
+                    $(this).parent().find('.choose1').removeClass('play');
+                }
+                /*---------------------------------*/
+                $(this).closest('.container').find('.step-one').css('width', '50%');
+                $(this).closest('.container').find('.step-second').css('width', '50%');
+                $(this).closest('.container').find('.step-second').removeClass('change');
+                $(this).closest('.container').find('.step-three').css('display', 'none');
+                /*------------------------------------*/
+
+                /*Set text span*/
+                $(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-left').text('Spese ritiro in negozio');
+                $(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-right').text('€ 0,00');
+                /*--------------------*/
+
+                let temp4 = $('#temp-store-country').html();
+                let render2 = mustache.render(temp4);
+                $('.choose-country').html(render2);
+            }
+        }
+
 
         $('.right').on('click', function(){
             $(this).parent().css("display", "none").parent().find('.shipping-mode-choose').css("display", "block");
-
+            $(this).closest('.shipping-mode').find('.shipping-mode-title').css('background-color', '#57a2ec');
         });
 
     });
 
-    
 
+    $('.count-product-right').on('click', function(){
+
+        /*let temp5 = $('#temp-show-products').html();
+        let render3 = mustache.render(temp5);
+        $('.show-product').html(render3);*/
+
+        if($(this).closest('.summary-product').find('.show-product').css('display')==='none'){
+            let temp5 = $('#temp-show-products').html();
+            let render3 = mustache.render(temp5);
+            $('.show-product').html(render3);
+            $(this).closest('.summary-product').find('.show-product').css('display', 'block');
+        }else{
+            $(this).closest('.summary-product').find('.show-product').css('display', 'none');
+        }
+
+    });
 
 
 });
--- Stage/checkoutPage/src/js/boundleEM5.js
+++ Stage/checkoutPage/src/js/boundleEM5.js
@@ -10599,34 +10599,96 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 				//$(this).parent().parent().find(".shipping-mode-title").css("background-color", "blue");
 				$(this).closest('.shipping-mode').find(".shipping-mode-title").css("background-color", "#0061af"); //cambia colore al titolo
 
-
-				if ($(this).find('.check1').find('input').is(':checked')) {
-					$(this).find('.check1').find('input').prop('checked', false);
-				} else {
-					$(this).find('.check1').find('input').prop('checked', true);
-				}
-
-				//$(this).find(".check1").find('input').prop("checked", true);//dà la proprietà checked alla checkbox
-				$(this).closest('.shipping').find('.shipping-address').find('.shipping-address-form').css("display", "block"); //display block al div contenitore della form
-
 				$(this).parent().css("display", "none"); //fa scomparire la scelta del tipo di ritiro
 
+				if ($(this).hasClass('choose1')) {
+					var temp = $('#temp-mustache').html();
+					var html = mustache.render(temp);
+					$('.shipping-address-form').html(html);
+					$(this).closest('.shipping-mode').find('.shipping-store-confirm').css("display", "none");
+					$(this).closest('.shipping-mode').find('.shipping-mode-confirm').css("display", "block");
 
-				$(this).closest('.shipping-mode').find('.shipping-mode-confirm').css("display", "block"); //mi fa comparire il tipo di spedizione scelto
+					$(this).find(".check1").find('input').prop("checked", true); //dà la proprietà checked alla checkbox
 
+					$(this).closest('.shipping').find('.shipping-address').find('.shipping-address-form').css("display", "block");
+					$(this).closest('.shipping').find('.shipping-address').find('.choose-country').css('display', 'none');
 
-				var temp = $('#temp-mustache').html();
-				var html = mustache.render(temp);
-				$('.shipping-address-form').html(html); //rendering form spedizione
+					/*---------------------------------*/
+					$(this).closest('.container').find('.step-one').css('width', '33.33%');
+					$(this).closest('.container').find('.step-second').css('width', '33.33%');
+					$(this).closest('.container').find('.step-second').addClass('change');
+					$(this).closest('.container').find('.step-three').css('display', 'block');
+					/*------------------------------------*/
 
+					/*Set text span*/
+					$(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-left').text('Spese di spedizione');
+					$(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-right').text('€ 4,99');
+					/*--------------------*/
 
-				var temp2 = $('#temp-choose-confirm').html();
-				var html2 = mustache.render(temp2);
-				$('.shipping-mode-confirm').html(html2); //rendering tipo di spedizione scelta
+					if ($(this).parent().find('.choose2').find('input').is(':checked')) {
+						$(this).parent().find('.choose2').find('input').prop('checked', false);
+						$(this).parent().find('.choose2').removeClass('play');
+					}
+
+					var temp2 = $('#temp-choose-confirm').html();
+					var html2 = mustache.render(temp2);
+					$('.shipping-mode-confirm').html(html2);
+				} else {
+					if ($(this).hasClass('choose2')) {
+						var temp3 = $('#temp-choose-store').html();
+						var render = mustache.render(temp3);
+						$('.shipping-store-confirm').html(render);
+
+						$(this).closest('.shipping-mode').find('.shipping-store-confirm').css("display", "block");
+						$(this).closest('.shipping-mode').find('.shipping-mode-confirm').css("display", "none");
+
+						$(this).closest('.shipping').find('.shipping-address').find('.shipping-address-form').css("display", "none");
+						$(this).closest('.shipping').find('.shipping-address').find('.choose-country').css("display", "block");
+
+						$(this).find(".check1").find('input').prop("checked", true); //dà la proprietà checked alla checkbox
+
+						if ($(this).parent().find('.choose1').find('input').is(':checked')) {
+							$(this).parent().find('.choose1').find('input').prop('checked', false);
+							$(this).parent().find('.choose1').removeClass('play');
+						}
+						/*---------------------------------*/
+						$(this).closest('.container').find('.step-one').css('width', '50%');
+						$(this).closest('.container').find('.step-second').css('width', '50%');
+						$(this).closest('.container').find('.step-second').removeClass('change');
+						$(this).closest('.container').find('.step-three').css('display', 'none');
+						/*------------------------------------*/
+
+						/*Set text span*/
+						$(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-left').text('Spese ritiro in negozio');
+						$(this).closest('.content').find('.cont-right').find('.product-container').find('.service-price').find('.internal-right').text('€ 0,00');
+						/*--------------------*/
+
+						var temp4 = $('#temp-store-country').html();
+						var render2 = mustache.render(temp4);
+						$('.choose-country').html(render2);
+					}
+				}
 
 				$('.right').on('click', function () {
 					$(this).parent().css("display", "none").parent().find('.shipping-mode-choose').css("display", "block");
+					$(this).closest('.shipping-mode').find('.shipping-mode-title').css('background-color', '#57a2ec');
 				});
+			});
+
+			$('.count-product-right').on('click', function () {
+
+				/*let temp5 = $('#temp-show-products').html();
+    let render3 = mustache.render(temp5);
+    $('.show-product').html(render3);*/
+
+				if ($(this).closest('.summary-product').find('.show-product').css('display') === 'none') {
+					var temp5 = $('#temp-show-products').html();
+					var render3 = mustache.render(temp5);
+					$('.show-product').html(render3);
+					$(this).closest('.summary-product').find('.show-product').css('display', 'block');
+				} else {
+					$(this).closest('.summary-product').find('.show-product').css('display', 'none');
+				}
 			});
 		});
 	}, { "Mustache": 1, "jquery": 2 }] }, {}, [3]);
