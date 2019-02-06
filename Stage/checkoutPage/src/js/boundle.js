d06b3deb493db0b598f27993c23b88ffd0a70361
silviobarbato <silvio.barbato@soprasteria.com>
2019-02-06 13:23:19 +0100

    fixed sintax sass and jquery

--- Stage/checkoutPage/src/js/boundle.js
+++ Stage/checkoutPage/src/js/boundle.js
@@ -11052,14 +11052,18 @@ return jQuery;
 var $ = window.jQuery = require('jquery');
 const mustache = require('Mustache');
 
+let sumProd = $('.summary-product');
+let container = $('.container');
+let content = $('.content');
+
 let detailsProducts = function(){
-    if($('.summary-product').find('.show-product').hasClass('hide')){
+    if(sumProd.find('.show-product').hasClass('hide')){
         let temp5 = $('#temp-show-products').html();
         $('.show-product').html(mustache.render(temp5));
 
-        $('.summary-product').find('.show-product').addClass('show').removeClass('hide');
+        sumProd.find('.show-product').removeClass('hide');
     } else {
-        $('.summary-product').find('.show-product').addClass('hide').removeClass('show');
+        sumProd.find('.show-product').addClass('hide')
     }
 };
 
@@ -11071,28 +11075,31 @@ let showChoose = function (th){
     th.parent().addClass('hide');
 
     if(th.hasClass('choose1')){
+
+
         let temp = $('#temp-mustache').html();
         $('.shipping-address-form').html(mustache.render(temp));
 
-        $('.shipping-store-confirm').addClass('hide').removeClass('show');
-        $('.shipping-mode-confirm').addClass('show').removeClass('hide');
+        $('.shipping-store-confirm').addClass('hide');
+        $('.shipping-mode-confirm').removeClass('hide');
 
         //dà la proprietà checked alla checkbox
         th.find('.check1 input').prop("checked", true);
 
-        $('.shipping-address-form').addClass('show').removeClass('hide');
-        $('.choose-country').addClass('hide').removeClass('show');
+        $('.shipping-address-form').removeClass('hide');
+        $('.choose-country').addClass('hide');
 
         /*---------------------------------*/
-        $('.container').find('.step-one').removeClass('fifty-fifty').addClass('thirty-three');
-        $('.container').find('.step-second').removeClass('fifty-fifty').addClass('thirty-three');
-        $('.container').find('.step-second').addClass('change');
-        $('.container').find('.step-three').addClass('show').removeClass('hide');
+        container.find('.step-one').addClass('thirty-three');
+        container.find('.step-second').addClass('thirty-three');
+        container.find('.step-second').addClass('change');
+        container.find('.step-three').removeClass('hide');
         /*------------------------------------*/
 
         /*Set text span*/
-        $('.content').find('.cont-right .product-container .service-price .internal-left').text('Spese di spedizione');
-        $('.content').find('.cont-right .product-container .service-price .internal-right').text('€ 4,99');
+        content.find('.cont-right .product-container .service-price .internal-left').text('Spese di spedizione');
+        content.find('.cont-right .product-container .service-price .internal-right').text('€ 4,99');
+        content.find('.cont-right .product-container .total-price .internal-right').text('€ 21,98');
         $('.shipping-address').find('.shipping-address-title .title-string').text('Aggiungi un indirizzo di spedizione');
         /*--------------------*/
 
@@ -11104,15 +11111,16 @@ let showChoose = function (th){
         let temp2 = $('#temp-choose-confirm').html();
         $('.shipping-mode-confirm').html(mustache.render(temp2));
 
-    }else{
+    } else {
+        
         if(th.hasClass('choose2')){
             let temp3 = $('#temp-choose-store').html();
             $('.shipping-store-confirm').html(mustache.render(temp3));
 
-            $('.shipping-mode').find('.shipping-store-confirm').addClass('show').removeClass('hide');
-            $('.shipping-mode').find('.shipping-mode-confirm').removeClass('show').addClass('hide');
-            $('.shipping').find('.shipping-address .shipping-address-form').addClass('hide').removeClass('show');
-            $('.shipping').find('.shipping-address .choose-country').addClass('show').removeClass('hide');
+            $('.shipping-mode').find('.shipping-store-confirm').removeClass('hide');
+            $('.shipping-mode').find('.shipping-mode-confirm').addClass('hide');
+            $('.shipping').find('.shipping-address .shipping-address-form').addClass('hide');
+            $('.shipping').find('.shipping-address .choose-country').removeClass('hide');
 
             th.find(".check1 input").prop("checked", true);
 
@@ -11121,15 +11129,16 @@ let showChoose = function (th){
                 th.parent().find('.choose1').removeClass('play');
             }
             /*---------------------------------*/
-            $('.container').find('.step-one').addClass('fifty-fifty').removeClass('thirty-three');
-            $('.container').find('.step-second').addClass('fifty-fifty').removeClass('thirty-three');
-            $('.container').find('.step-second').removeClass('change');
-            $('.container').find('.step-three').addClass('hide').removeClass('show');
+            container.find('.step-one').removeClass('thirty-three');
+            container.find('.step-second').removeClass('thirty-three');
+            container.find('.step-second').removeClass('change');
+            container.find('.step-three').addClass('hide');
             /*------------------------------------*/
 
             /*Set text span*/
-            $('.content').find('.cont-right .product-container .service-price .internal-left').text('Spese ritiro in negozio');
-            $('.content').find('.cont-right .product-container .service-price .internal-right').text('€ 0,00');
+            content.find('.cont-right .product-container .service-price .internal-left').text('Spese ritiro in negozio');
+            content.find('.cont-right .product-container .service-price .internal-right').text('€ 0,00');
+            content.find('.cont-right .product-container .total-price .internal-right').text('€ 16,99');
             $('.shipping-address').find('.shipping-address-title .title-string').text('Selziona il punto vendita');
             /*--------------------*/
 
@@ -11145,14 +11154,16 @@ let showChoose = function (th){
 };
 
 var pippo = function ($this){
-    $this.parent().removeClass('show').addClass('hide');
-    $('.shipping').find('.shipping-mode-choose').removeClass('hide').addClass('show');
+    $this.parent().addClass('hide');
+    $('.shipping').find('.shipping-mode-choose').removeClass('hide');
     $('.shipping-mode').find('.shipping-mode-title').toggleClass('active');
 };
 
 $( document ).ready(function() {
 
-    $('.count-product').on('click', detailsProducts);
+    $('.count-product').on('click', function(){
+        detailsProducts();
+    });
 
     $('.choose').on('click',function(){
         var th = $(this);
